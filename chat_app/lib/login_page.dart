959af4ac8f06847c88cae094f450cf1415bcb55a
akpt <athul.k@in.zlinkcorp.com>
2025-06-13 15:39:05 +0530

    Login page and chatapp widget
    Learning path

---
+++ chat_app/lib/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(backgroundColor: Colors.green,
+        ),
+        drawer: Drawer(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+          print('Button Clicked');
+        }),
+        body: Text('Hello Anju'),
+      );
+  }
+}
